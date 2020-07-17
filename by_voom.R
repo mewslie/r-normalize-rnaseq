5f20581f783a1a8abe16d36ce098ea6bf3da7209
mewslie <52728006+mewslie@users.noreply.github.com>
2020-07-17 16:51:35 +0900

    Update by_voom.R

--- by_voom.R
+++ by_voom.R
@@ -1,24 +1,24 @@
 #manually enter paths, files, file attributes etc
 file.index <- file.path("C:\\File\\Path\\Here", "filename.csv") #sequence counts
-experiment <- data.frame(sample=c("BF1","BF2","AF1","AF2","BM1","BM2","AM1","AM2"), group=rep(rep(c("Before","After"),each=2),2), gender=rep(c("Female","Male"), each=4))
-experiment$group <- relevel(as.factor(experiment$group), ref="Before")
-experiment$gender <- relevel(as.factor(experiment$gender), ref="Female")
+experiment <- data.frame(sample = c("BF1","BF2","AF1","AF2","BM1","BM2","AM1","AM2"), group = rep(rep(c("Before","After"), each = 2), 2), gender = rep(c("Female","Male"), each = 4))
+experiment$group <- relevel(as.factor(experiment$group), ref = "Before")
+experiment$gender <- relevel(as.factor(experiment$gender), ref = "Female")
 design <- model.matrix(~ experiment$group * experiment$gender) #last factor entered in the formula should be the main factor of interest
 colnames(design) <- c("Int","BvsA","MvsF","GroupVGender")
 design.group.size <- 2 #no. of samples in comparison group eg GroupVGender
 low.count.threshold <- 1 #no. of counts per million that a gene is required to have per sample to stay in this analysis
 
 #load input data
-counts.dat <- read.csv(file.index, row.names=1)
+counts.dat <- read.csv(file.index, row.names = 1)
 
 #remove sequences with too many low counts
 library("edgeR")
-keep <- rowSums(cpm(counts.dat) > low.count.threshold)>=design.group.size; table(keep)
+keep <- rowSums(cpm(counts.dat) > low.count.threshold) > =design.group.size; table(keep)
 counts.keep <- counts.dat[keep,]
 ###graph counts.keep###
-counts.keep.lcpm <- cpm(counts.keep, log=TRUE)
-boxplot(counts.keep.lcpm, xlab="", ylab="Log2 counts per million (CPM)", las=2, outline=FALSE, main="logCPM (unnormalised counts)")
-abline(h=median(counts.keep.lcpm),col="blue") #mark median logCPM
+counts.keep.lcpm <- cpm(counts.keep, log = TRUE)
+boxplot(counts.keep.lcpm, xlab = "", ylab = "Log2 counts per million (CPM)", las = 2, outline = FALSE, main = "logCPM (unnormalised counts)")
+abline(h = median(counts.keep.lcpm), col = "blue") #mark median logCPM
 
 #use calcNormFactors and voom on counts.keep
 counts.dge <- DGEList(counts.keep)
